1ebcf10ab13de76a3b3cebeaeaa0d61aa1895ce1
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-12 19:23:46 +0200

    first pass at updating controllers

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,30 +1,18 @@
 import { CommentType, Comment } from '../models/comment.model';
-import { MongoClient, Repository } from 'mongoize-orm';
+import { Repository } from 'mongoize-orm';
 
-export async function createComment(
-  client: MongoClient,
-  payload: CommentType
-): Promise<Comment> {
-  return new Comment().build(payload).save(client);
+export async function createComment(payload: CommentType): Promise<Comment> {
+  return new Comment().build(payload).save();
 }
 
-export async function findComment(
-  client: MongoClient,
-  id: string
-): Promise<Comment> {
-  return Repository.with(Comment).findById(client, id);
+export async function findComment(id: string): Promise<Comment> {
+  return Repository.with(Comment).findById(id);
 }
 
-export async function findComments(
-  client: MongoClient,
-  query: object
-): Promise<Comment[]> {
-  return Repository.with(Comment).findMany(client, query);
+export async function findComments(query: object): Promise<Comment[]> {
+  return Repository.with(Comment).findMany(query);
 }
 
-export async function deleteComment(
-  client: MongoClient,
-  id: string
-): Promise<Comment> {
-  return Repository.with(Comment).deleteOne(client, id);
+export async function deleteComment(id: string): Promise<Comment> {
+  return Repository.with(Comment).deleteOne(id);
 }
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,47 +1,32 @@
 import { UserType, User } from '../models/user.model';
-import { MongoClient, Repository } from 'mongoize-orm';
+import { Repository } from 'mongoize-orm';
 
-export async function createUser(
-  client: MongoClient,
-  payload: UserType
-): Promise<User> {
-  const user = await Repository.with(User).findOne(client, {
-    email: payload.email
-  });
+export async function createUser(payload: UserType): Promise<User> {
+  const user = await Repository.with(User).findOne({ email: payload.email });
   if (user) {
     return user;
   }
 
-  return new User().build(payload).save(client);
+  return new User().build(payload).save();
 }
 
-export async function findUser(client: MongoClient, id: string): Promise<User> {
-  const user = await Repository.with(User).findById(client, id);
+export async function findUser(id: string): Promise<User> {
+  const user = await Repository.with(User).findById(id);
   if (user) {
     return user;
   }
 
-  throw new Error('user not found');
+  throw new Error('User not found');
 }
 
-export async function findUsers(
-  client: MongoClient,
-  query: object
-): Promise<User[]> {
-  return Repository.with(User).findMany(client, query);
+export async function findUsers(query: object): Promise<User[]> {
+  return Repository.with(User).findMany(query);
 }
 
-export async function updateUser(
-  client: MongoClient,
-  id: string,
-  payload: object
-): Promise<User> {
-  return Repository.with(User).updateOne(client, id, payload);
+export async function updateUser(id: string, payload: object): Promise<User> {
+  return Repository.with(User).updateOne(id, payload);
 }
 
-export async function deleteUser(
-  client: MongoClient,
-  id: string
-): Promise<User> {
-  return Repository.with(User).deleteOne(client, id);
+export async function deleteUser(id: string): Promise<User> {
+  return Repository.with(User).deleteOne(id);
 }
